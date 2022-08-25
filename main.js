8a2d3ab454a8998722862c72aa2cf904f52cb6b8
Josiah Aye <josiahaye@gmail.com>
2022-08-24 18:24:11 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("my first console project");
+var first_name="poopoo :)";
+var last_name="toilet:)";
+var full_name=first_name.concat(last_name);
+
+console.log(full_name);
+var a= 10000000;
+var b= 10000000;
+var add= a+b;
+
+console.log(add);
+var sub= a-b
+
+console.log(sub);
+var multiply= a*b;
+
+console.log(multiply);
+var divide= a/b;
+
+console.log(divide);
